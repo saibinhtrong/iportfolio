27c25b59fff5919388cb3dcade83122073a221e3
binhtrong4354t <saibinhtrong18122003@gmail.com>
2023-02-22 16:00:38 +0700

    html-css-login

--- main.js
+++ main.js
@@ -5,14 +5,15 @@ import projectListAdmin from "./src/admin/projectListAdmin";
 import userListAdmin from "./src/admin/userListAdmin";
 import HomePage from "./src/client";
 import { render, router } from "./src/lib";
+import login from "./src/login";
 
 const app = document.querySelector("#app");
 
 router.on("/admin/*", () => {},{
         before(done){
-            // const user = JSON.parse(localStorage.getItem("user")) | [];
-        // if (!user) return (window.location.href = "/");
-        // if (user && user.id != "1") return (window.location.href = "/login");              
+            const user = JSON.parse(localStorage.getItem("user")) | [];
+        if (!user) return (window.location.href = "/");
+        if (user && user.id != "1") return (window.location.href = "/login");              
 
 
                 done();
@@ -24,5 +25,6 @@ router.on("/admin/projectListAdmin", ()=>render(projectListAdmin, app))
 router.on("/admin/projectAddAdmin", ()=>render(projectAddAdmin, app))
 router.on("/admin/projectEditAdmin/:id", ({data})=> render(()=>projectEditAdmin(data),app));
 router.on("/admin/userListAdmin",()=>render(userListAdmin, app))
+router.on("/login", () => render(login,app))
 router.resolve();
 
